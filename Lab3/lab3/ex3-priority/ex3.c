eb2b19d162d2206d273c39241843461bec27ecf5
Tan Yuanhong <leotan.yh@gmail.com>
2019-10-15 19:08:25 +0800

    lock next(next) segment and zip for lab3

--- Lab3/lab3/ex3-priority/ex3.c
+++ Lab3/lab3/ex3-priority/ex3.c
@@ -14,6 +14,8 @@
 #include <stdio.h>
 #define wait(i) pthread_mutex_lock(&mutex[i])
 #define signal(i) pthread_mutex_unlock(&mutex[i])
+#define next(i) NEXT(i, num_of_segments)
+#define prev(i) PREV(i, num_of_segments)
 
 //Using extern, you can use the global variables num_of_cars and num_of_segments from ex3_runner.c in your code.
 extern int num_of_cars;
@@ -21,18 +23,18 @@ extern int num_of_segments;
 
 // segment_struct** strs;
 pthread_mutex_t *mutex;
-pthread_mutex_t *isEmpty;
-sem_t sem;
+pthread_mutex_t *safeToEnter;
+sem_t limit_sem;
 
 void initialise()
 {
-    sem_init(&sem, 0, num_of_segments-1);
+    sem_init(&limit_sem, 0, num_of_segments-1);
     mutex = (pthread_mutex_t *)malloc((num_of_segments) * sizeof(pthread_mutex_t));
-	isEmpty = (pthread_mutex_t *) malloc ( (num_of_segments) * sizeof(pthread_mutex_t) );
+	safeToEnter = (pthread_mutex_t *) malloc ( (num_of_segments) * sizeof(pthread_mutex_t) );
     for (int i = 0; i < num_of_segments; i++)
     {
         pthread_mutex_init(&mutex[i], NULL);
-		pthread_mutex_init(&isEmpty[i], NULL);
+		pthread_mutex_init(&safeToEnter[i], NULL);
     }
 }
 
@@ -41,13 +43,15 @@ void cleanup()
     for (int i = 0; i < num_of_segments; i++)
     {
         pthread_mutex_destroy(&mutex[i]);
-	pthread_mutex_destroy(&isEmpty[i]);
+        pthread_mutex_destroy(&safeToEnter[i]);
     }
+    sem_destroy(&limit_sem);
     free(mutex);
-    free(isEmpty);
+    free(safeToEnter);
 }
 
-void *car(void *car)
+
+void* car(void* car)
 {
     //This function modeles a thread
     //A car:
@@ -56,25 +60,27 @@ void *car(void *car)
     //   -finally call exit_roundabout (...)
     car_struct *self = (car_struct *)car;
 
-    sem_wait(&sem);
-    pthread_mutex_lock(&isEmpty[self->entry_seg]);
-    pthread_mutex_unlock(&isEmpty[self->entry_seg]);
+    sem_wait(&limit_sem);
+    pthread_mutex_lock(&safeToEnter[self->entry_seg]);
     wait(self->entry_seg);
+    pthread_mutex_lock(&safeToEnter[next(self->entry_seg)]);
     enter_roundabout(self);
+    pthread_mutex_unlock(&safeToEnter[self->entry_seg]);
 
     while(self->current_seg != self->exit_seg) {
-	pthread_mutex_lock(&isEmpty[NEXT(self->current_seg, num_of_segments)]);
         wait(NEXT(self->current_seg, num_of_segments));
+	    if(next(self->current_seg) != self->exit_seg)
+            pthread_mutex_lock(&safeToEnter[next(next(self->current_seg))]);
         move_to_next_segment(self);
-        signal(PREV(self->current_seg, num_of_segments));
-	pthread_mutex_unlock(&isEmpty[PREV(self->current_seg, num_of_segments)]);
+        pthread_mutex_unlock(&safeToEnter[self->current_seg]);
+	    signal(prev(self->current_seg));
     }
 
     exit_roundabout(self);
     signal(self->exit_seg);
-    pthread_mutex_unlock(&isEmpty[self->exit_seg]);
+    // pthread_mutex_unlock(&safeToEnter[self->exit_seg]);
     // signal(PREV(self->current_seg, num_of_segments));
-    sem_post(&sem);
+    sem_post(&limit_sem);
 
     pthread_exit(NULL);
 }
