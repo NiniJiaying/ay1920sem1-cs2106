7a55a4a3676b0b78f6c6d45a04458d946159d9b1
Tan Yuanhong <leotan.yh@gmail.com>
2019-10-17 21:34:18 +0800

    ex2

--- Lab4/lab4/ex2.c
+++ Lab4/lab4/ex2.c
@@ -1,5 +1,5 @@
 /*************************************
- * Lab 4 Exercise 1
+ * Lab 4 Exercise 2
  * Name: Tan Yuanhong
  * Student No: A0177903X
  * Lab Group: 07
@@ -41,6 +41,16 @@ void os_run(int initial_num_pages, page_table *pg_table){
         int const requested_page = info.si_value.sival_int;
         
         if (requested_page == -1) break;
+
+        if (!(requested_page >= 0 && requested_page < initial_num_pages)) {
+            // tell the MMU that we are done updating the page table
+            union sigval reply_value;
+            // set to 0 if the page is successfully loaded, 
+            // set to 1 if the page is not mapped to the user process (i.e. segfault)
+            reply_value.sival_int = 1;
+            sigqueue(info.si_pid, SIGCONT, reply_value);
+            continue;
+        }
         
         // process the signal, and update the page table as necessary
         while(page_in_frame[victim] != -1 && pg_table->entries[victim].referenced == 1) {
@@ -50,6 +60,9 @@ void os_run(int initial_num_pages, page_table *pg_table){
         // printf("victim: %d\n", victim);
         if(page_in_frame[victim] != -1) {
             pg_table->entries[page_in_frame[victim]].valid = 0;
+        }
+        if(pg_table->entries[page_in_frame[victim]].dirty == 1) {
+            disk_write(victim, page_in_frame[victim]);
         }
         disk_read(victim, requested_page);
         pg_table->entries[requested_page].valid = 1;
